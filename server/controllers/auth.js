306426b6b2230b0a94eb6976c1be5b4da20d6be9
Anna An <arzumanyan.an@gmail.com>
2023-01-18 17:12:26 +0400

    hashing passwords

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 // Register user
 export const register = async (req, res) => {
@@ -11,6 +12,19 @@ export const register = async (req, res) => {
         message: "This username is taken.",
       });
     }
+
+    const salt = bcrypt.genSaltSync(10);
+    // we receive this password from the above req.body (const { username, password } = req.body;) abd difficulty is salt
+    const hash = bcrypt.hashSync(password, salt);
+    // this is a new example of the User model that we created in the models
+    // we set password to hash because we don't want the password to be saved in the db, instead we use the hashed pw
+    const newUser = new User({
+      username,
+      password: hash,
+    });
+
+    // we register this new user in the bd using mongoose
+    await newUser.save();
   } catch (err) {
     console.log(err);
   }
