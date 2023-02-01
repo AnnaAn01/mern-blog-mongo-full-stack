cee5feb2877ff91386aa5c4d6100684ca73df91c
Anna An <arzumanyan.an@gmail.com>
2023-02-01 17:57:21 +0400

    working on Login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const LoginPage = () => {
-  return <form onSubmit={(e) => e.preventDefault}>LoginPage</form>;
+  return (
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="w-1/4 h-60 mx-auto mt-40"
+    >
+      <h1 className="text-lg text-white text-center">Authorization</h1>
+      <label htmlFor="" className="text-xs text-gray-400">
+        Username:
+        <input
+          type="text"
+          placeholder="Username"
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-8 text-xs outline-none placeholder:tet-gray-700"
+        />
+      </label>
+    </form>
+  );
 };
